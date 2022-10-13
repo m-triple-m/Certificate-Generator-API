567156fac9874d27c674c07b4d9548796a51450b
m-triple-m <triplem656@mail.com>
2022-10-13 12:44:50 +0530

    added single certificate generation

--- routers/certificateRouter.js
+++ routers/certificateRouter.js
@@ -29,9 +29,9 @@ const makeZip = (cb, pathToFiles = "./static/generatedPDF") => {
   }
 };
 
-router.post("/generate", async (req, res) => {
+router.post("/generatebatch", async (req, res) => {
   // console.log(req.body);
-  const {sheetData, template_id} = req.body;
+  const { sheetData, template_id } = req.body;
   const template = await templateModel.findById(template_id);
   console.log(sheetData);
   const studentData = sheetData.slice(1);
@@ -69,6 +69,55 @@ router.post("/generate", async (req, res) => {
           zipFile: "Minor_Project_Certificates.zip",
         });
       });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post("/generatesingle", async (req, res) => {
+  // console.log(req.body);
+  const { certificateDetails, template_id } = req.body;
+  const template = await templateModel.findById(template_id);
+  console.log(certificateDetails);
+  // const studentData = sheetData.slice(1);
+  // const zip = new JSZip();
+  try {
+    // for (let student of studentData) {
+    let pdfGenerator = new PDFGenerator(template.file);
+
+    // zip.folder("Minor_Project_Certificates");
+    const pdfData = await pdfGenerator.modify(certificateDetails);
+    let pdFileName = certificateDetails.studentName.trim().replaceAll(" ", "_")+'.pdf';
+    fs.writeFile(`./static/generatedPDF/${pdFileName}`, pdfData, (err) => {
+      if (err) throw err;
+      console.log("It's saved!");
+      res.json({
+        status: "success",
+        pdfFile: pdFileName,
+      });
+    });
+    // zip.file(
+    //   `Minor_Project_Certificates/${student[2]
+    //     .trim()
+    //     .replaceAll(" ", "_")}.pdf`,
+    //   pdfData
+    // );
+    // }
+
+    //   zip
+    //     .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+    //     .pipe(
+    //       fs.createWriteStream(
+    //         `./static/generatedPDF/Minor_Project_Certificates.zip`
+    //       )
+    //     )
+    //     .on("finish", () => {
+    //       console.log("zipped!!");
+    //       res.json({
+    //         status: "success",
+    //         zipFile: "Minor_Project_Certificates.zip",
+    //       });
+    //     });
   } catch (err) {
     console.error(err);
   }
